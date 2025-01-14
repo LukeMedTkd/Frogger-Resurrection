3c6723dca97ff437a25c6aa8c7fc6ddd340a1cb8
corrazz <c.casu47@studenti.unica.it>
2025-01-14 20:02:32 +0100

    defined pseudode

--- game.c
+++ game.c
@@ -1,3 +1,6 @@
+#include "game.h"
+#include "struct.h"
+
 /*
 1. creazione iniziale frog -> inserimento in una lista con FROG_ID perchè? va ucciso il processo
 2. creazione iniziale dei crocodilles
@@ -8,3 +11,42 @@ for i=0; i < n_manches, i++
 
 
  */
+void start_game(){
+    //set game variables
+    game_var gameVar;
+    gameVar.manche = MANCHES;
+    gameVar.score = SCORE;
+    gameVar.time = TIME;
+
+    //CreateProcess(pipe,listpid,void (*Frog_process)(int, int*),int* params)->Creazione processo RANA
+    //CreateProcess(pipe,listpid,void (*Time_process)(int, int*),int* params)->Creazione processo TEMPO
+    for (int i = 0; i < MANCHES; i++)
+    {
+        //reset default position-> posizione rana, tempo, score
+        //Randomizzare velocità e settare direzione dei flussi
+        for (int i = 0; i < 8; i++)
+        {
+            //CreateProcess(pipe,listpid,void (*Crocodile_process)(int, int*),int* params))->Creazione processo Coccodrillo
+            //CrocodileProcess(int y, int velocità di creazione,direzione del flusso)
+        }
+        /*ProcessoControllo
+        -Legge da pipe i messaggi->controlla da quale entità sono stai mandati e li stampa a schermo
+        -Stampa Area di gioco
+        -Collisioni:
+            Rana:
+            -Cade nell'acqua->perde vita e manche
+            -Rana sopra il coccodrillo
+            -Rana nella tana già occupata->perde vita e manche
+            -Rana sparata dal coccodrillo->perde vita e manche
+            -Rana va dietro le tane->perde vita e manche
+            -Rana raggiunge la tana->finisce la manche e aumenta il punteggio
+            Proiettile:
+            -Proiettile contro proiettile-> kill
+            -Proiettile che esce dallo schermo-> kill
+            Coccodrillo:
+            -se esce dallo schermo->ricicla processi*****
+        */
+        
+    }
+    
+}
