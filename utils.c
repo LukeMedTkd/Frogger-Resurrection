048d1f6a4cfb81aec06dedf15bcface58f7d1cab
LukeMedTkd <lucamedda191@gmail.com>
2025-01-14 16:38:14 +0100

    FIXED COLORS PROBLEMS

--- utils.c
+++ utils.c
@@ -14,6 +14,7 @@ void init_colors(){
 
     // Enable colors
     start_color();
+    use_default_colors();
 
     init_color(COLOR_BLACK, 0, 0, 0);
     init_color(COLOR_WHITE, 255, 255, 255);
@@ -24,20 +25,20 @@ void init_colors(){
     init_color(COLOR_MAGENTA, 255, 0, 255);
     init_color(COLOR_CYAN, 0, 255, 255);
     init_color(COLOR_FROG, 55, 166, 43);
-    init_color(COLOR_RIVER, 50, 90, 160);
+    init_color(COLOR_RIVER, 215, 570, 800);
     init_color(COLOR_CROCODILLE, 29, 87, 23);
-    init_color(COLOR_DENS, 110, 70, 10);
-    init_color(COLOR_GRASS, 10, 110, 20);
+    init_color(COLOR_DENS, 568, 250, 205);
+    init_color(COLOR_GRASS, 245, 570, 200);
     init_color(COLOR_SCORE, 25, 25, 25);
     
 
     // (text, bg)
-    init_pair(COLOR_FROG_ID, COLOR_BLACK, COLOR_FROG);
-    init_pair(COLOR_RIVER_ID, COLOR_BLACK, COLOR_RIVER);
-    init_pair(COLOR_CROCODILLE_ID, COLOR_BLACK, COLOR_CROCODILLE);
-    init_pair(COLOR_DENS_ID, COLOR_BLACK, COLOR_DENS);
-    init_pair(COLOR_SCORE_ID, COLOR_BLACK, COLOR_SCORE); 
-    init_pair(COLOR_GRASS_ID, COLOR_BLACK, COLOR_GRASS);     
+    init_pair(COLOR_FROG_ID, COLOR_WHITE, COLOR_FROG);
+    init_pair(COLOR_RIVER_ID, COLOR_WHITE, COLOR_RIVER);
+    init_pair(COLOR_CROCODILLE_ID, COLOR_WHITE, COLOR_CROCODILLE);
+    init_pair(COLOR_DENS_ID, COLOR_WHITE, COLOR_DENS);
+    init_pair(COLOR_SCORE_ID, COLOR_WHITE, COLOR_SCORE); 
+    init_pair(COLOR_GRASS_ID, COLOR_WHITE, COLOR_GRASS);     
 
 }
 
@@ -45,7 +46,7 @@ void init_colors(){
 void print_game_area(){
     // Dens
     attron(COLOR_PAIR(COLOR_DENS_ID));
-    for (int y = SCORE_HEIGHT; y < DENS_HEIGTH+SCORE_HEIGHT; y++){
+    for (int y = SCORE_HEIGHT; y < GRASS_Y_SUP_START; y++){
         for (int x = 0; x < COLS; x++) {mvwaddch(stdscr, y, x, ' ');}
     }
     attroff(COLOR_PAIR(COLOR_DENS_ID));
@@ -60,7 +61,8 @@ void print_game_area(){
     // River
     attron(COLOR_PAIR(COLOR_RIVER_ID));
     for (int y = RIVER_Y_START; y < RIVER_Y_END; y++){
-        for (int x = 0; x < COLS; x++) {mvwaddch(stdscr, y, x, ' ');}
+        // for (int x = 0; x < COLS; x++) {mvwaddch(stdscr, y, x, ' ');}
+        mvhline(y, 0, ' ', COLS);
     }
     attroff(COLOR_PAIR(COLOR_RIVER_ID));
 
@@ -70,7 +72,7 @@ void print_game_area(){
         for (int x = 0; x < COLS; x++) {mvwaddch(stdscr, y, x, ' ');}
     }
     attroff(COLOR_PAIR(COLOR_GRASS_ID));
-
+    mvprintw(LINES, 4, "cioa");
     refresh();
     sleep(20);
     endwin();
