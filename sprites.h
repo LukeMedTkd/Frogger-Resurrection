209bbc31f19a825499cae500bb210c263da87a3c
LukeMedTkd <lucamedda191@gmail.com>
2025-01-21 11:48:08 +0100

    Add crocodile dim

--- sprites.h
+++ sprites.h
@@ -1,5 +1,14 @@
 #include "struct.h"
 
+// Frog constants
+#define FROG_DIM_X 6
+#define FROG_DIM_Y 3
+
+
+// Crocodile constants
+#define CROCODILE_DIM_X
+#define CROCODILE_DIM_Y
+
 // Functions Prototypes
 void print_game_area(WINDOW *game);
 void print_frog(WINDOW *game, Character frog_entity);
